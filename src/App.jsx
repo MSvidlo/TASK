15f68abccd3d6f20d5ffb42e8e292528ef3aad0e
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-08-15 14:13:15 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,22 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import  ContactFrom from './components/ContactForm/ContactForm';
-import ContacList from './components/ContactList/ContactList';
-import SearchBox from './components/SearchBox/SearchBox';
-import { fetchContacts } from './redux/contactsOps';
+import HomePage from './pages/HomePage.Jsx';
+
 
 const App = () => {
   const dispatch = useDispatch();
-  const selectLoading = useSelector((state) => state.contacts.isLoading);
-  const selectError= useSelector((state)=>state.contacts.error)
-  useEffect(() => {
-    dispatch(fetchContacts())
-  }, [dispatch]);
+  const isRefresh = useSelector(selectIsRefreshing);
 
+  useEffect(() => {
+    dispatch(refreshing());
+  }, [dispatch]);
   return (
     <div>
-      <h1>Phonebook</h1>
-      <ContactFrom />
+      <h1>Campers</h1>
+  <HomePage/>
        {selectLoading && !selectError && <b>Request in progress...</b>}
-      <SearchBox />
-      <ContacList />
+     
     </div>
   );
 };
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+
+import styles from "./HomePage.module.css";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className={styles.HomePageSection}>
+      <title>Home Page</title>
+      
+    </div>
+  );
+};
+
+export default HomePage;
+
